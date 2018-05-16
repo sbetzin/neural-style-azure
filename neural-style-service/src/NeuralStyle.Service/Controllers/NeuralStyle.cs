c75b8b98cd795001fa66dc273518b69610cb60ae
Sebastian Betzin <sebastian.betzin@generic.de>
2018-05-16 16:33:46 +0200

    added docker

---
+++ neural-style-service/src/NeuralStyle.Service/Controllers/NeuralStyle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NeuralStyle.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class NeuralStyle : Controller
+    {
+        // GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpGet]
+        public string Get([FromQuery] string source, [FromQuery] string style)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}

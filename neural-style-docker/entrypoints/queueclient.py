8bbd16b5303fb2ad91a15af1b9899048eb882c64
Sebastian Betzin <sebastian.betzin@generic.de>
2018-05-18 16:26:15 +0200

    added call to styletransfer

--- neural-style-docker/entrypoints/queueclient.py
+++ neural-style-docker/entrypoints/queueclient.py
@@ -5,6 +5,7 @@ import json
 import base64
 from azure.storage.queue import QueueService
 from azure.storage.blob import BlockBlobService
+from neuralstyle.algorithms import styletransfer
 
 queue_service = QueueService(account_name='neuralstylefiles', account_key='mMxv0dYg1xyEqE5VsrZejnH1PKQL5NsvG2gwYAfyHCrN1LDGYTXztCLoyfXa7ObB9BpPvXhGBtBg2A6owaV3gQ==')
 blob_service = BlockBlobService(account_name='neuralstylefiles', account_key='mMxv0dYg1xyEqE5VsrZejnH1PKQL5NsvG2gwYAfyHCrN1LDGYTXztCLoyfXa7ObB9BpPvXhGBtBg2A6owaV3gQ==')
@@ -26,6 +27,8 @@ def handle_message(message):
         blob_service.get_blob_to_path("images", styleId, file_path= "/app/images/style.jpg")
 
         print('start job with SourceId=' + sourceId + ', StyleId='+ styleId)
+        styletransfer(["/app/images/source.jpg"], ["/app/images/style.jpg"], "app/images/out.jpg", 1200, "gatys", 500, [50], [1], 1700, 100, [1], None)
+
     except Exception as e:
         print(e)
 

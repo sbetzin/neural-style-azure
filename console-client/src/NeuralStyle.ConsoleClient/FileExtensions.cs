d25dee0e8e644097b507ad1e276181f64b155549
Sebastian Betzin <sebastian.betzin@generic.de>
2018-05-31 13:30:18 +0200

    upload of files simplified

--- console-client/src/NeuralStyle.ConsoleClient/FileExtensions.cs
+++ console-client/src/NeuralStyle.ConsoleClient/FileExtensions.cs
@@ -8,14 +8,24 @@ namespace NeuralStyle.ConsoleClient
 {
     public static class FileExtensions
     {
-        public static async Task<string> UploadToBlob(this string file, CloudBlobContainer container)
+        public static async Task UploadToBlob(this string file, CloudBlobContainer container)
         {
             var name = Path.GetFileName(file);
             var blob = container.GetBlockBlobReference(name);
 
+            if (blob.ExistsAsync().Result)
+            {
+                var info = new FileInfo(file);
+
+                if (info.Length == blob.Properties.Length)
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine($"   Uploading {file}");
+
             await blob.UploadFromFileAsync(file);
-        
-            return name;
         }
 
         public static IEnumerable<string> GetFiles(this string fileOrFolder)
--- console-client/src/NeuralStyle.ConsoleClient/Program.cs
+++ console-client/src/NeuralStyle.ConsoleClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -25,46 +26,61 @@ namespace NeuralStyle.ConsoleClient
             var queue = queueClient.GetQueueReference("jobs");
             queue.EncodeMessage = false;
 
-            var images = blobClient.GetContainerReference("images");
+            var blobContainer = blobClient.GetContainerReference("images");
 
-            var kandinskyStyles = Directory.GetFiles(@"C:\Data\images\style\", "kandinsky_*.jpg");
-            var modernArtStyle = Directory.GetFiles(@"C:\Data\images\style\", "modern_art_*.jpg");
-            var danceStyles = Directory.GetFiles(@"C:\Data\images\style\", "elena_prokopenko_*.jpg");
-            var goghStyles = Directory.GetFiles(@"C:\Data\images\style\", "gogh_*.jpg");
-            var afremovStyles = Directory.GetFiles(@"C:\Data\images\style\", "Leonid_afremov*.jpg");
-            var comicStyles = Directory.GetFiles(@"C:\Data\images\style\", "comic_*.jpg");
-            var picassoStyles = Directory.GetFiles(@"C:\Data\images\style\", "picasso_*.jpg");
-            var corinth = Directory.GetFiles(@"C:\Data\images\style\", "lovis_corinth_*.jpg");
+            var stylePath = @"C:\Data\images\style";
+            var inPath = @"C:\Data\images\in";
 
-            var ana = Directory.GetFiles(@"C:\Data\images\in\", "ana*.jpg");
-            var sebastian = Directory.GetFiles(@"C:\Data\images\in\", "sebastian_*.jpg");
+            var kandinskyStyles = Directory.GetFiles(stylePath, "kandinsky_*.jpg");
+            var modernArtStyle = Directory.GetFiles(stylePath, "modern_art_*.jpg");
+            var danceStyles = Directory.GetFiles(stylePath, "elena_prokopenko_*.jpg");
+            var goghStyles = Directory.GetFiles(stylePath, "gogh_*.jpg");
+            var afremovStyles = Directory.GetFiles(stylePath, "Leonid_afremov*.jpg");
+            var comicStyles = Directory.GetFiles(stylePath, "comic_*.jpg");
+            var picassoStyles = Directory.GetFiles(stylePath, "picasso_*.jpg");
+            var corinth = Directory.GetFiles(stylePath, "lovis_corinth_*.jpg");
+            var allStyles = Directory.GetFiles(stylePath, "*.jpg");
 
-            //CreateBatch(images, queue, @"C:\Data\images\in\messe.jpg", @"C:\Data\images\style\kandinsky_schwarz-und-violett.jpg", 500, 1500, 50.0, 1, 800, 100, false).Wait();
-            CreateJobs(images, queue, ana, picassoStyles, 500, 750, 50.0, 1, 800, 100, true).Wait();
+            var ana = Directory.GetFiles(inPath, "ana*.jpg");
+            var sebastian = Directory.GetFiles(inPath, "sebastian_*.jpg");
+
+            var newPics = new[] { $@"{inPath}\kraemerbruecke.jpg" };
+
+            RunIt(blobContainer, queue, newPics, allStyles, 500, 1500, 50.0, 1, 1500, 100, true);
         }
 
-        private static async Task CreateBatch(CloudBlobContainer images, CloudQueue queue, string source, string style, int iterations, int size, double styleWeight, double styleScale, int tileSize, int tileOverlap, bool useOriginalColors)
+        private static void RunIt(CloudBlobContainer blobContainer, CloudQueue queue, string[] images, string[] styles, int iterations, int size, double styleWeight, int styleScale, int tileSize, int tileOverlap, bool useOriginalColors)
         {
-            var sourceFiles = source.GetFiles();
-            var styleFiles = style.GetFiles();
+            UploadImages(blobContainer, images);
+            UploadImages(blobContainer, styles);
 
-            await CreateJobs(images, queue, sourceFiles, styleFiles, iterations, size, styleWeight, styleScale, tileSize, tileOverlap, useOriginalColors);
+            CreateJobs(queue, images, styles, iterations, size, styleWeight, styleScale, tileSize, tileOverlap, useOriginalColors).Wait();
         }
 
-        private static async Task CreateJobs(CloudBlobContainer images, CloudQueue queue, IEnumerable<string> sourceFiles, IEnumerable<string> styleFiles, int iterations, int size, double styleWeight, double styleScale, int tileSize, int tileOverlap, bool useOriginalColors)
+        private static void UploadImages(CloudBlobContainer blobContainer, string[] images)
         {
-            foreach (var (sourceFile, styleFile) in sourceFiles.Product(styleFiles))
+            Console.WriteLine($"checking {images.Length} images for upload");
+            foreach (var image in images)
             {
-                await CreateJob(images, queue, sourceFile, styleFile, iterations, size, styleWeight, styleScale, useOriginalColors, tileSize, tileOverlap);
+                image.UploadToBlob(blobContainer).Wait();
             }
         }
 
-        private static async Task CreateJob(CloudBlobContainer images, CloudQueue queue, string source, string style, int iterations, int size, double styleWeight, double styleScale, bool useOriginalColors, int tileSize, int tileOverlap)
-        {
-            var sourceName = source.UploadToBlob(images).Result;
-            var styleName = style.UploadToBlob(images).Result;
 
-            var job = new Job { SourceName = sourceName, StyleName = styleName, Iterations = iterations, Size = size, StyleWeight = styleWeight, StyleScale = styleScale, UseOriginalColors = useOriginalColors, TileSize = tileSize, TileOverlap = tileOverlap };
+        private static async Task CreateJobs(CloudQueue queue, IEnumerable<string> sourceFiles, IEnumerable<string> styleFiles, int iterations, int size, double styleWeight, double styleScale, int tileSize, int tileOverlap, bool useOriginalColors)
+        {
+            var jobs = sourceFiles.Product(styleFiles).ToList();
+
+            Console.WriteLine($"Creating {jobs.Count} jobs");
+            foreach (var (sourceFile, styleFile) in jobs)
+            {
+                await CreateJob(queue, sourceFile, styleFile, iterations, size, styleWeight, styleScale, useOriginalColors, tileSize, tileOverlap);
+            }
+        }
+
+        private static async Task CreateJob(CloudQueue queue, string sourceFile, string styleFile, int iterations, int size, double styleWeight, double styleScale, bool useOriginalColors, int tileSize, int tileOverlap)
+        {
+            var job = new Job { SourceName = Path.GetFileName(sourceFile), StyleName = Path.GetFileName(styleFile), Iterations = iterations, Size = size, StyleWeight = styleWeight, StyleScale = styleScale, UseOriginalColors = useOriginalColors, TileSize = tileSize, TileOverlap = tileOverlap };
             job.TargetName = CreateTargetName(job);
 
             var json = JsonConvert.SerializeObject(job);
@@ -72,7 +88,7 @@ namespace NeuralStyle.ConsoleClient
 
             await queue.AddMessageAsync(message);
 
-            Console.WriteLine($"Created job for {source} with style {style}");
+            Console.WriteLine($"   added job for image {sourceFile} with style {styleFile}");
         }
 
         private static string CreateTargetName(Job job)

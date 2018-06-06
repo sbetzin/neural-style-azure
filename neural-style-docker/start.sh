87a093a2551c726b9b8f4fb7be759bd7d0e38249
Sebastian Betzin <sebastian.betzin@generic.de>
2018-06-06 15:12:51 +0200

    fixed multiple env passing

--- neural-style-docker/start.sh
+++ neural-style-docker/start.sh
@@ -1,1 +1,1 @@
-sudo nvidia-docker run -d -e AzureStorageConnectionString TileSize --name neural-style --restart=unless-stopped  sbetzin/neural-style
+sudo nvidia-docker run -d -e AzureStorageConnectionString -e TileSize --name neural-style --restart=unless-stopped  sbetzin/neural-style

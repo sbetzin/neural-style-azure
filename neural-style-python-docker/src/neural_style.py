bbe5f919043079a6aa5654abe0195a7de76495bd
Sebastian Betzin <sebastian.betzin@generic.de>
2018-06-08 19:02:41 +0200

    rf

--- neural-style-python-docker/src/neural_style.py
+++ neural-style-python-docker/src/neural_style.py
@@ -595,10 +595,10 @@ def stylize(content_img, style_imgs, init_img, frame=None):
         output_img = convert_to_original_colors(np.copy(content_img), output_img)
         write_video_output(frame, output_img)
     else:
+      output_img_original_color = output_img = convert_to_original_colors(np.copy(content_img), output_img)
+
       write_image(os.path.join(args.img_output_dir, args.img_name.replace("#origcolor#","0")), output_img)
-      
-      output_img = convert_to_original_colors(np.copy(content_img), output_img)
-      write_image(os.path.join(args.img_output_dir, args.img_name.replace("#origcolor#","1")), output_img)
+      write_image(os.path.join(args.img_output_dir, args.img_name.replace("#origcolor#","1")), output_img_original_color)
 
 def minimize_with_lbfgs(sess, net, optimizer, init_img):
   if args.verbose: print('\nMINIMIZING LOSS USING: L-BFGS OPTIMIZER')

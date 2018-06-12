74a3bff6b2a3f1ddbfe1b40553aed07652862228
Sebastian Betzin <sebastian.betzin@generic.de>
2018-06-12 18:44:04 +0200

    changed docker image name

--- docker/neural-style-tensorflow-docker/start.sh
+++ docker/neural-style-tensorflow-docker/start.sh
@@ -1,1 +1,1 @@
-sudo nvidia-docker run -d -e AzureStorageConnectionString --name neural-style-python --restart=unless-stopped  sbetzin/neural-style-python
+sudo nvidia-docker run -d -e AzureStorageConnectionString --name neural-style-tensorflow --restart=unless-stopped  sbetzin/neural-style-tensorflow

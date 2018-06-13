746e4519179b6867c837a84d92aaed709d0db5b2
Sebastian Betzin <sebastian.betzin@generic.de>
2018-06-13 15:11:29 +0200

    updated test case for utf-16-le encoding

--- docker/neural-style-tensorflow-docker/src/exifdump.py
+++ docker/neural-style-tensorflow-docker/src/exifdump.py
@@ -3,24 +3,26 @@ import piexif
 import array
 import utils
 
-exif_dict = piexif.load("C:/Data/Development/Private/neural-style-azure/test.jpg")
+en = u"ana"
+test =  bytearray(en, "utf-16-le")
+exif_dict = piexif.load("C:/Data/test.jpg")
 
 comment = bytearray(exif_dict["0th"][piexif.ImageIFD.XPComment])
 keywords = bytearray(exif_dict["0th"][piexif.ImageIFD.XPKeywords])
 
-print (comment.decode("utf16").rstrip('\x00'))
-print (keywords.decode("utf16").rstrip('\x00'))
+# print (comment.decode("utf16").rstrip('\x00'))
+# print (keywords.decode("utf16").rstrip('\x00'))
 
 
-new_keywords = bytearray("content=ana.jpg,style=van_gogh.jpg,iterations=500".encode("utf16"))
+new_keywords = bytearray("content=ana.jpg,style=van_gogh.jpg,iterations=500".encode("utf-16-le"))
 exif_dict["0th"][piexif.ImageIFD.XPKeywords] = new_keywords
 
 newexif = {"0th": {}, "Exif" : {}, "GPS" : {}, "1st" : {}}
 newexif["0th"][piexif.ImageIFD.XPKeywords] = new_keywords
-newexif["0th"][piexif.ImageIFD.XPAuthor] = bytearray("tensorflow neural style".encode("utf16"))
-newexif["0th"][piexif.ImageIFD.XPTitle] = bytearray("ana.jpg".encode("utf16"))
-newexif["0th"][piexif.ImageIFD.XPSubject] = bytearray("van_gogh_die_sterne.jpg".encode("utf16"))
-newexif["0th"][piexif.ImageIFD.XPComment] = bytearray("iteration=500,size=700,cw=5,sw=100".encode("utf16"))
+newexif["0th"][piexif.ImageIFD.XPAuthor] = bytearray("tensorflow neural style".encode("utf-16-le"))
+newexif["0th"][piexif.ImageIFD.XPTitle] = bytearray("ana.jpg".encode("utf-16-le"))
+newexif["0th"][piexif.ImageIFD.XPSubject] = bytearray("van_gogh_die_sterne.jpg".encode("utf-16-le"))
+newexif["0th"][piexif.ImageIFD.XPComment] = bytearray("iteration=500,size=700,cw=5,sw=100".encode("utf-16-le"))
 
 exif_bytes = piexif.dump(newexif)
-piexif.insert(exif_bytes, "C:/Data/Development/Private/neural-style-azure/_test/test2.jpg")
+piexif.insert(exif_bytes, "C:/Data/test.jpg")

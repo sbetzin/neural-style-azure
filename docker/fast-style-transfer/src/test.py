7963ce98e6a4443febb58620b00256e15f09c219
Sebastian Betzin <sebastian.betzin@generic.de>
2019-12-27 23:35:07 +0100

    added test for fast style transfer with prepared model

---
+++ docker/fast-style-transfer/src/test.py
@@ -0,0 +1,90 @@
+# Copyright 2019 The TensorFlow Hub Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import functools
+import os
+from matplotlib import gridspec
+import matplotlib.pylab as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from PIL import Image
+
+print("TF-Hub version: ", hub.__version__)
+print("Eager mode enabled: ", tf.executing_eagerly())
+print("GPU available: ", tf.test.is_gpu_available())
+
+
+def save_file(image):
+  #jpgeg_encoded = tf.image.encode_jpeg(image)
+  target_file_name = "c:\\data\\test.jpg"
+
+  image = Image.fromarray(image, "RGB")
+  with open(target_file_name, "wb") as target_file:
+          target_file.write(image)
+
+
+def crop_center(image):
+  """Returns a cropped square image."""
+  shape = image.shape
+  new_shape = min(shape[1], shape[2])
+  offset_y = max(shape[1] - shape[2], 0) // 2
+  offset_x = max(shape[2] - shape[1], 0) // 2
+  image = tf.image.crop_to_bounding_box(
+      image, offset_y, offset_x, new_shape, new_shape)
+  return image
+
+def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
+  """Loads and preprocesses images."""
+  # Cache image file locally.
+  image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+  img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
+  if img.max() > 1.0:
+    img = img / 255.
+  if len(img.shape) == 3:
+    img = tf.stack([img, img, img], axis=-1)
+  img = crop_center(img)
+  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+
+  return img
+
+
+content_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg'
+style_image_url = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg'
+output_image_size = 384  
+
+# The content image size can be arbitrary.
+content_img_size = (output_image_size, output_image_size)
+# The style prediction model was trained with image size 256 and it's the 
+# recommended image size for the style image (though, other sizes work as 
+# well but will lead to different results).
+style_img_size = (256, 256)  # Recommended to keep it at 256.
+
+content_image = load_image(content_image_url, content_img_size)
+style_image = load_image(style_image_url, style_img_size)
+style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
+
+# Load TF-Hub module.
+hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+hub_module = hub.load(hub_handle)
+
+# Stylize content image with given style image.
+# This is pretty fast within a few milliseconds on a GPU.
+outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+stylized_image = outputs[0]
+
+
+save_file(stylized_image)

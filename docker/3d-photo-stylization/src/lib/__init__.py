4ce2e2823e97f40fac0561759afc6e3632495af7
Sebastian Betzin <sebastian.betzin@generic.de>
2023-04-17 20:00:23 +0200

    removed old project

--- docker/3d-photo-stylization/src/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import config
-from . import data
-from . import trainer
-from . import util
-from . import camera
--- docker/3d-photo-stylization/src/lib/render/src/binding.cpp
+++
@@ -1,9 +0,0 @@
-#include "render.h"
-
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("rasterize", &rasterize);
-    m.def("refine_z_buffer", &refine_z_buffer);
-    m.def("splat", &splat);
-    m.def("splat_grad", &splat_grad);
-}

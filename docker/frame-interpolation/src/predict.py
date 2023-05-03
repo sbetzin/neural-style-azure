1956dd69d443318c6580c478ca750376cfd2e521
Sebastian Betzin <sebastian.betzin@generic.de>
2023-05-03 12:06:05 +0200

    changed predictor with sampe code

--- docker/frame-interpolation/src/predict.py
+++ docker/frame-interpolation/src/predict.py
@@ -1,36 +1,14 @@
-import os
-from pathlib import Path
-import numpy as np
-import tempfile
-import tensorflow as tf
-import mediapy
-from PIL import Image
-import cog
+from cog import BasePredictor, Path, Input
+import torch
 
-from eval import interpolator, util
-
-
-class Predictor(cog.Predictor):
+class Predictor(BasePredictor):
     def setup(self):
-       print("setup done")
-       
-    @cog.input(
-        "frame1",
-        type=Path,
-        help="The first input frame",
-    )
-    @cog.input(
-        "frame2",
-        type=Path,
-        help="The second input frame",
-    )
-    @cog.input(
-        "times_to_interpolate",
-        type=int,
-        default=1,
-        min=1,
-        max=8,
-        help="Controls the number of times the frame interpolator.",
-    )
-    def predict(self, frame1, frame2, times_to_interpolate):
-        print("DONE")
+        """Load the model into memory to make running multiple predictions efficient"""
+        print("Done setup")
+
+    def predict(self,
+            #image: Path = Input(description="Image to enlarge"),
+            scale: float = Input(description="Factor to scale image by", default=1.5)
+    ) -> Path:
+        # ... post-processing ...
+        return "Done"

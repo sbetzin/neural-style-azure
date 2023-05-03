692bdfa99738b36d0f54a061cb55714502749d1f
Sebastian Betzin <sebastian.betzin@generic.de>
2023-05-03 12:24:16 +0200

    moved the creation of interpolator

--- docker/frame-interpolation/src/predict.py
+++ docker/frame-interpolation/src/predict.py
@@ -3,9 +3,6 @@ from eval import interpolator as interpolator_lib
 
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-        self.interpolator = interpolator_lib.Interpolator("/pretrained_models/film_net/Style/saved_model", None, [block_height, block_width])
-
         print("Done setup")
 
     def predict(self,
@@ -15,6 +12,9 @@ class Predictor(BasePredictor):
             block_height: int = Input(description="Block height" , default=1),
             block_width: int = Input(description="Block width" , default=1),
     ) -> str:
+        
+        interpolator = interpolator_lib.Interpolator("/pretrained_models/film_net/Style/saved_model", None, [block_height, block_width])
+
         # ... post-processing ...
         return "Done"
     

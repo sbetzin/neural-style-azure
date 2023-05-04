8649c6364a9934c820341c212c6000d7510bd6c3
Sebastian Betzin <sebastian.betzin@generic.de>
2023-05-04 22:38:45 +0200

    updated queue client

--- docker/3d-photo-inpainting/src/queueclient.py
+++ docker/3d-photo-inpainting/src/queueclient.py
@@ -8,18 +8,14 @@ import os.path
 import logging
 import argparse
 import yaml
+import shutil
 
 from azure.storage.queue import QueueClient
-from azure.storage.blob import BlobServiceClient, BlobClient
 from azure.core.exceptions import ResourceExistsError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("queueclient")
 logger.setLevel(logging.INFO)
-
-# Only show error messages for the azure storage. otherwise the console is spammed with debug messages
-azure_logger = logging.getLogger("azure.storage")
-azure_logger.setLevel(logging.ERROR)
 
 # Only show error messages for the azure core. otherwise the console is spammed with debug messages
 azure_logger = logging.getLogger("azure.core")
@@ -27,11 +23,6 @@ azure_logger.setLevel(logging.ERROR)
 
 #insights.enable_logging()
 #telemetrie = insights.create_telemetrie_client()
-
-def ensure_dir(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
 
 def prepare_queue(queue_client, queue_name):
     try:
@@ -42,15 +33,7 @@ def prepare_queue(queue_client, queue_name):
     except Exception as e:
         logger.exception(e)
 
-def prepare_blob(blob_service_client, container_name):
-    try:
-        blob_service_client.create_container(container_name)
-    except ResourceExistsError:
-        logger.info("Container already exists.")
-    except Exception as e:
-        logger.exception(e)
-
-def handle_message(blob_service_client, message):
+def handle_message(message):
     try:
         logger.info("--------------------------------------------------------------------------------------------------")
         logger.info("handling new message %s", message.id )
@@ -62,38 +45,46 @@ def handle_message(blob_service_client, message):
         recreate_depth_mesh = job["recreate_depth_mesh"]
         depth_mode = job["depth_mode"]
         
-        directory_content = "image"
-        directory_result = "video"
-        directory_mesh = f"/mesh/{depth_mode}"
-        directory_depth = f"/depth/{depth_mode}"
+        image_folder = "/nft/in"
+        local_image_folder = "/image"
+        video_folder = "/nft/video"
+        mesh_folder = f"/mesh/{depth_mode}"
+        depth_folder = f"/depth/{depth_mode}"
         
-        job["mesh_folder"] = directory_mesh
-        job["depth_folder"] = directory_depth
-         
-        os.makedirs(directory_content, exist_ok=True)
-        os.makedirs(directory_result, exist_ok=True)
+        job["mesh_folder"] = mesh_folder
+        job["depth_folder"] = depth_folder
+        job["video_folder"] = video_folder
         
-        content_file = os.path.join(directory_content, content_name)
+        os.makedirs(local_image_folder, exist_ok=True)
+        os.makedirs(video_folder, exist_ok=True)
         
-        # Delete all existing images. Otherwise the 3d-inpainting would iterate them all
-        clear_directory(directory_content)
-        handle_mesh_deletion(directory_depth, directory_mesh, content_name, recreate_depth_mesh)
+        content_file = find_image_file(image_folder, content_name)
+        local_content_file = os.path.join(local_image_folder, content_name)
+        
+        # Delete all local existing images. Otherwise the 3d-inpainting would iterate them all
+        clear_directory(local_image_folder)
+        handle_mesh_deletion(depth_folder, mesh_folder, content_name, recreate_depth_mesh)
         update_yaml_file('default.yml', job)
         
-        logger.info("downloading %s", content_file)
-        download_file(blob_service_client, content_name, content_file)
+        logger.info(f"copying {content_file} to {local_content_file}")
+        shutil.copyfile(content_file, local_content_file)
 
-        logger.info("start 3d-inpainting with Content=%s", content_name)
+        logger.info(f"start 3d-inpainting with Content={content_name}")
         command = f'python main.py --config default.yml'
         os.system(command)
  
         logger.info("Setting exif data")
         #exifdump.write_exif(out_file_origcolor_0, config)
-        
-        upload_videos(blob_service_client, directory_result)    
-        upload_depth(blob_service_client, directory_depth, replace_file_extension(content_name, '.png'), depth_mode)    
     except Exception as e:
         logger.exception(e)
+
+def find_image_file(folder_path, content_name):
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(".jpg") and file == content_name:
+                return os.path.join(root, file)
+
+    return None
 
 def handle_mesh_deletion(directory_depth, directory_mesh, content_name, recreate_depth_mesh):
     if not recreate_depth_mesh:
@@ -130,59 +121,23 @@ def clear_directory(target_path):
     for file in files:
         os.remove(file)
 
-def upload_videos(blob_service_client, directory_result):
-    files = glob.glob(os.path.join(directory_result, "*.mp4"))
-    
-    for file in files:
-        target_name = os.path.basename(file)
-        print(f"Uploading {file}")
-        upload_file(blob_service_client,"results", target_name, file)
-        os.remove(file)
-
-def upload_depth(blob_service_client, directory_depth, depth_file_name, depth_mode):
-    print(f"Uploading {depth_file_name}")
-    depth_file = os.path.join(directory_depth, depth_file_name)
-    container_file = f"{depth_mode}/{depth_file_name}"
-    
-    upload_file(blob_service_client, "depth", container_file, depth_file)
-     
-def download_file(blob_service_client, source_name, source_file):
-    blob_client = blob_service_client.get_blob_client(container="images", blob=source_name)
-    
-    with open(source_file, "wb") as download_file:
-        download_file.write(blob_client.download_blob().readall())
-
-def upload_file(blob_service_client, container, target_name, file_name):
-    try:
-        if os.path.exists(file_name):
-            logger.info ("uploading file %s", file_name)
-            
-            blob_client = blob_service_client.get_blob_client(container=container, blob=target_name)
-            with open(file_name, "rb") as data:
-                blob_client.upload_blob(data, overwrite=True)
-
-        else:
-            logger.info("file %s does not exit", file_name)
-    except Exception as e:
-        logger.exception(e)
-
-def poll_queue(queue_client, blob_service_client):
+def poll_queue(queue_client):
     try:
         while True:
-            CheckQueue(queue_client, blob_service_client)
+            CheckQueue(queue_client)
                 
             time.sleep(5)
     except Exception as e:
         logger.exception(e)
 
-def CheckQueue(queue_client, blob_service_client):
+def CheckQueue(queue_client):
     messages = queue_client.receive_messages(messages_per_page=1, visibility_timeout=30*60)
 
     for message_batch in messages.by_page():
         for message in message_batch:
             start_time = time.time()
 
-            handle_message(blob_service_client, message)
+            handle_message(message)
             measure_time(start_time)
 
             queue_client.delete_message(message)
@@ -197,14 +152,6 @@ def setup_azure_queue(azure_connection_string, queue_name):
         logger.exception(e)
 
     return queue_client
-
-def setup_azure_blob(azure_connection_string):
-    try:
-        blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-    except Exception as e:
-        logger.exception(e)
-
-    return blob_service_client
 
 def update_yaml_file(yaml_file, new_values):
     # YAML-Datei einlesen
@@ -248,15 +195,12 @@ def main(argv):
     queue_name ="jobs-3d-inpainting"
 
     queue_client = setup_azure_queue(azure_connection_string, queue_name)
-    blob_service_client = setup_azure_blob(azure_connection_string)
   
     logger.info ("preparing azure resources")
     prepare_queue(queue_client, queue_name)
-    prepare_blob(blob_service_client, "images")
-    prepare_blob(blob_service_client, "results")
 
     logger.info ("starting to poll jobs queue: %s", queue_name)
-    poll_queue(queue_client, blob_service_client)
+    poll_queue(queue_client)
 
 if __name__ == '__main__':
   main(sys.argv[1:])

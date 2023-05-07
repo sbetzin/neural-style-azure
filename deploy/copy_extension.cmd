d7b28378ecb126b1dea68ef6a2e9a7a8e4f1deea
Sebastian Betzin <sebastian.betzin@generic.de>
2023-05-07 16:45:10 +0200

    rm cached

--- deploy/copy_extension.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-xcopy C:\Data\Development\Private\neural-style-azure\src\NeuralStyle.ExplorerExtension\bin\Debug\*.exe C:\Data\Development\Private\neural-style-azure\deploy\ExplorerExtension\ /Y
-xcopy C:\Data\Development\Private\neural-style-azure\src\NeuralStyle.ExplorerExtension\bin\Debug\*.dll C:\Data\Development\Private\neural-style-azure\deploy\ExplorerExtension\ /Y
-xcopy C:\Data\Development\Private\neural-style-azure\src\NeuralStyle.ExplorerExtension\bin\Debug\*.config C:\Data\Development\Private\neural-style-azure\deploy\ExplorerExtension\ /Y
-
-pause

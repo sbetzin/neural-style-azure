316608172ccac4f3dddd18779e770afdb4c684f7
Sebastian Betzin <sebastian.betzin@generic.de>
2023-08-28 18:48:01 +0200

    changed to windows

--- src/NeuralStyle.Tests/ExplorerExtensionTests.cs
+++ src/NeuralStyle.Tests/ExplorerExtensionTests.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using NeuralStyle.Core;
 using NeuralStyle.Core.Folders;
-using NeuralStyle.ExplorerExtension;
 using Xunit;
 
 namespace NeuralStyle.Tests
@@ -16,11 +15,11 @@ namespace NeuralStyle.Tests
         [Fact]
         public static void TestLargeImage()
         {
-            var menu = new ExplorerMenu();
+            //var menu = new ExplorerMenu();
 
-            var result = FolderCheck.IsInFolder(@"C:\Users\gensb\OneDrive\_nft\in\lofoten-8.jpg", BasePathes.InPath());
+            //var result = FolderCheck.IsInFolder(@"C:\Users\gensb\OneDrive\_nft\in\lofoten-8.jpg", BasePathes.InPath());
 
-            result = FolderCheck.IsInFolder(@"C:\Users\gensb\OneDrive\_nft\out\kroatien_05-eugene_ivanov_gentleman_with_newspaper-1000px_cw_30000_sw_100000_tv_10_model_vgg19_opt_lbfgs_origcolor_0.jpg", BasePathes.InPath());
+            //result = FolderCheck.IsInFolder(@"C:\Users\gensb\OneDrive\_nft\out\kroatien_05-eugene_ivanov_gentleman_with_newspaper-1000px_cw_30000_sw_100000_tv_10_model_vgg19_opt_lbfgs_origcolor_0.jpg", BasePathes.InPath());
         }
 
     }
